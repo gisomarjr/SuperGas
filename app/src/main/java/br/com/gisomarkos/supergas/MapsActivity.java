d7aaa3efd0471ce3813fc6e90da0477380266440
Gisomar <gisomarjr1994@gmail.com>
2015-08-01 19:06:46 -0300

    Finalizando latitude camera e o local definido

--- app/src/main/java/br/com/gisomarkos/supergas/MapsActivity.java
+++ app/src/main/java/br/com/gisomarkos/supergas/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 
@@ -21,7 +22,10 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
 
     double latitude;
     double longitude;
-    LatLng mapCenter;
+    LatLng meuLocal;
+    LatLng outroLocal;
+    private Marker marker;
+    GoogleMap _map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +39,21 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
 
     }
 
+    public void addMarker(LatLng latLng, String title, String snippet){
+        MarkerOptions options = new MarkerOptions();
+        options.position(latLng).title(title).snippet(snippet).draggable(true);
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.pin));
+
+        marker = _map.addMarker(options);
+    }
+
+
     @Override
     public void onMapReady(GoogleMap map) {
 
         GPS gps = new GPS(this);
+
+        this._map = map;
 
         if(gps.canGetLocation()){
             latitude = gps.getLatitude();
@@ -51,19 +66,48 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
         }
 
         //seta a localização atual no mapa
-        mapCenter = new LatLng(latitude, longitude);
+        meuLocal = new LatLng(latitude, longitude);
+        outroLocal = new LatLng(-8.060483, -34.901429);
+        //mapCenter = new LatLng(-8.060483, -34.901429);
 
         //movendo a camera para localização atual
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, 13));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(meuLocal, 13));
 
         // You can customize the marker image using images bundled with
         // your app, or dynamically generated bitmaps.
-        final Marker makerMap = map.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icone_gas_pequeno))
-                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
-                .title("Fornecedor")
-                .snippet("Nome: Gisomar")
-                .position(new LatLng(-8.060483,-34.901429)));
+
+
+        // EVENTS - mudou a posicao da camera? pega latitude e longitude
+        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                Log.i("Script", "setOnCameraChangeListener()");
+
+                    LatLng latitudeCamera = new LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude);
+                    if(distance(latitudeCamera,outroLocal) < 1000) {
+                        addMarker(outroLocal, "Fornecedor", "Gisomar");
+                        Toast.makeText(getApplicationContext(),"chegou no local",Toast.LENGTH_LONG).show();
+                    }
+					/*if(marker != null){
+						marker.remove();
+					}
+					customAddMarker(new LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude), "1: Marcador Alterado", "O Marcador foi reposicionado");
+					*/
+            }
+        });
+
+           //se a distancia do local atual for menor  do que algum metro definido - adicionar o marcador no mapa
+         /*   if(distance(meuLocal,outroLocal) <= 1000) {
+
+                final Marker makerMap = map.addMarker(new MarkerOptions()
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.icone_gas_pequeno))
+                        .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                        .title("Fornecedor")
+                        .snippet("Nome: Gisomar")
+                        .position(outroLocal));//-8.060483, -34.901429
+                Toast.makeText(this,"distancia: " + distance(meuLocal,outroLocal) + "metros",Toast.LENGTH_LONG).show();
+
+            }*/
 
         map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
 
@@ -78,7 +122,7 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
         map.setMyLocationEnabled(true);
 
         CameraPosition cameraPosition = CameraPosition.builder()
-                .target(mapCenter)
+                .target(meuLocal)
                 .zoom(13)
                 .bearing(200)
                 .build();
@@ -86,5 +130,17 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
         // Animate the change in camera view over 3 seconds
         map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),
                 3000, null);
+    }
+
+    public static double distance(LatLng StartP, LatLng EndP) {
+        double lat1 = StartP.latitude;
+        double lat2 = EndP.latitude;
+        double lon1 = StartP.longitude;
+        double lon2 = EndP.longitude;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLon = Math.toRadians(lon2-lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return 6366000 * c;
     }
 }
